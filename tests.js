7e2eaadfd74678b3960e7fc131133817d1bd52f4
Thomas Parslow <tom@almostobsolete.net>
2014-10-10 12:25:02 +0100

    Initial commit

---
+++ tests.js
@@ -0,0 +1,96 @@
+var through2Concurrent = require('./through2-concurrent');
+var expect = require('expect.js');
+var _ = require('underscore');
+
+var oldNextTick = process.nextTick;
+
+
+describe('through2-concurrent', function () {
+  var nextTickScheduled, collectingThrough, transformCalls, flushCalls;
+
+  function runNextTicks () {
+    var execute = function (fn) {
+        fn();
+    };
+    while (nextTickScheduled.length) {
+      var fns = nextTickScheduled;
+      nextTickScheduled = [];
+      fns.forEach(execute);
+    }
+  }
+
+  beforeEach(function () {
+    nextTickScheduled = [];
+    process.nextTick = function (fn) {
+      nextTickScheduled.push(fn);
+    };
+  });
+
+  describe('object streams', function () {
+    beforeEach(function () {
+      transformCalls = [];
+      flushCalls = [];
+
+      collectingThrough = through2Concurrent.obj(
+        {maxConcurrency: 4},
+        function (chunk, enc, callback) {
+          transformCalls.push({chunk: chunk, enc: enc, callback: callback});
+        },function (callback) {
+          flushCalls.push({callback: callback});
+        });
+    });
+    
+    it('should initiially allow the maximum specified concurrency', function () {
+      _.times(10, function (i) {
+        collectingThrough.write({number: i});
+      });
+      runNextTicks();
+      expect(transformCalls.length).to.be(4);
+    });
+
+    it('should feed more through once the concurrency drops below the given limit', function () {
+      _.times(10, function (i) {
+        collectingThrough.write({number: i});
+      });
+      runNextTicks();
+      transformCalls[2].callback();
+      transformCalls[0].callback();
+      runNextTicks();
+      expect(transformCalls.length).to.be(6);
+    });
+
+    it('should wait for all transform calls to finish before running flush', function () {
+      _.times(10, function (i) {
+        collectingThrough.write({number: i});
+      });
+      collectingThrough.end();
+      runNextTicks();
+      _.times(9, function (i) {
+        transformCalls.pop().callback();
+        runNextTicks();
+      });
+      expect(transformCalls.length).to.be(1);
+      expect(flushCalls.length).to.be(0);
+      transformCalls[0].callback();
+      expect(flushCalls.length).to.be(1);
+    });
+
+    it('should wait for everything to complete before emitting "end"', function () {
+      var endCalled = false;
+      collectingThrough.on('end', function () {
+        endCalled = true;
+      });
+      collectingThrough.resume();
+      collectingThrough.write({hello: 'world'});
+      collectingThrough.end();
+      runNextTicks();
+      expect(endCalled).to.be(false);
+      transformCalls[0].callback();
+      runNextTicks();
+      expect(endCalled).to.be(false);
+      flushCalls[0].callback();
+      runNextTicks();
+      expect(endCalled).to.be(true);
+    });
+  });
+});
